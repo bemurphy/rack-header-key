1e35ad82390dae0dfa9d612e9a736255318b5a95
Brendon Murphy <xternal1+github@gmail.com>
2011-09-02 15:58:04 -0700

    Specs refactoring.

--- spec/header_key_spec.rb
+++ spec/header_key_spec.rb
@@ -1,6 +1,19 @@
 require 'spec_helper'
 
-describe Rack::HeaderKey do
+RSpec::Matchers.define :be_allowed do
+  match do |response|
+    response.status.should == 200
+    response.body.should == "success"
+  end
+end
+
+RSpec::Matchers.define :be_unauthorized do
+  match do |response|
+    response.status.should == 401
+  end
+end
+
+describe "Requests using Rack::HeaderKey" do
   let(:key) { "sekret" }
   let(:app) {
     Rack::Builder.new do
@@ -9,7 +22,7 @@ describe Rack::HeaderKey do
     end
   }
 
-  it "raises an argument error if given a path option that doesn't start with /" do
+  it "raise an argument error if the initialization path option doesn't start with /" do
     app = Rack::Builder.new do
       use Rack::HeaderKey, :key => "sekret", :path => "bad_path"
       run lambda { |env| [200, {'Content-Type' => "text/plain"}, ["success"]]}
@@ -21,40 +34,37 @@ describe Rack::HeaderKey do
   end
 
   context "for the protected path" do
-    it "returns 200 if the proper key is present in X_AUTHORIZATION_KEY" do
+    it "are allowed if the proper key is present in X_AUTHORIZATION_KEY" do
       response = Rack::MockRequest.new(app).get('/api/test', "X_AUTHORIZATION_KEY" => key)
-      response.status.should == 200
-      response.body.should == "success"
+      response.should be_allowed
     end
 
-    it "returns 401 if the the proper key is not in X_AUTHORIZATION_KEY" do
+    it "are unauthorized if the the proper key is not in X_AUTHORIZATION_KEY" do
       response = Rack::MockRequest.new(app).get('/api/test', "X_AUTHORIZATION_KEY" => "bogus_key")
-      response.status.should == 401
+      response.should be_unauthorized
     end
   end
 
   context "for an unprotected path" do
-    it "returns 200 when no key is given" do
+    it "are allowed when no key is given" do
       response = Rack::MockRequest.new(app).get('/test')
-      response.status.should == 200
-      response.body.should == "success"
+      response.should be_allowed
     end
 
-    it "returns 200 even if an improper key is given" do
+    it "are allowed even if an improper key is given" do
       response = Rack::MockRequest.new(app).get('/test', "X_AUTHORIZATION_KEY" => "bogus_key")
-      response.status.should == 200
-      response.body.should == "success"
+      response.should be_allowed
     end
   end
 
   context "when no path is given" do
-    it "protects the root path" do
+    it "the root path is protected" do
       app = Rack::Builder.new do
         use Rack::HeaderKey, :key => "sekret"
         run lambda { |env| [200, {'Content-Type' => "text/plain"}, ["success"]]}
       end
       response = Rack::MockRequest.new(app).get('/test', "X_AUTHORIZATION_KEY" => "bogus_key")
-      response.status.should == 401
+      response.should be_unauthorized
     end
   end
 end
